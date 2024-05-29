1abc32e33e60746415d865b195ca6b6374078227
Stephenmuhindi <bteneson42@gmail.com>
2024-05-29 11:47:49 -0700

    hashed password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Password hashing and validation functions """
+import bcrypt
+
+def hash_password(password: str) -> bytes:
+  """Hashes password with bcrypt (returns digest)"""
+  return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+  """Checks password against hashed password (returns True if match)"""
+  return bcrypt.checkpw(password.encode(), hashed_password)
+
