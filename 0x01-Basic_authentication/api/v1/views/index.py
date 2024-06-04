e2bc831f1361557dabc5f88aacac76478c4801a6
Stephenmuhindi <bteneson42@gmail.com>
2024-06-04 05:52:11 -0700

    se

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""
+module doc
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,19 +8,13 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
+    """ get api status"""
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
+    """ get object stats """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
@@ -28,16 +23,11 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def abort_401() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - test unauthorized endpoint
-    """
+    """ get api unauthorized"""
     abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def abort_403() -> str:
-    """
- Z
-    """
+    """ get api forbiden """
     abort(403)
